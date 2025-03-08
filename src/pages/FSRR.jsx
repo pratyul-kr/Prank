8a64392ebd0743fac535c5def7732eed9ebb9226
Pratyul Kumar Rath <pratyulrath1234@gmail.com>
2025-03-08 02:27:01 +0000

    Updated reset button

--- src/pages/FSRR.jsx
+++ src/pages/FSRR.jsx
@@ -15,7 +15,7 @@ function FSRR() {
         <input placeholder="Registration Number"></input>
         <input placeholder="DOB"></input>
         <Link className={styles.view} onClick={() => setShowElement(true)}>View</Link>
-        <Link className={styles.reset}>Reset</Link>
+        <Link className={styles.reset} onClick={() => setShowElement(false)}>Reset</Link>
       </div>
        {showElement && <img className={styles.hehe} src={hehe} alt="Example" />}
     </main>
